5ad84bafd2c803945fbc21d9c91a0aeb3c036acd
URETZ <u-beresnev2006@mail.ru>
2026-03-12 21:34:30 +0700

    Персоздал проект C#

---
+++ L3S4/Element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+struct Element
+{
+    int TreeNumber;
+    string Name;
+    string MethodName = string.Empty;
+
+    public  Element (int TreeNumber, string Name, String MethodName = null)
+    {
+        this.TreeNumber = TreeNumber;
+        this.Name = Name;
+        if (MethodName != null) this.MethodName = MethodName;
+    }
+}
