d85f6f851c5313ef5dd7682d8296bd66db2e9de3
URETZ <u-beresnev2006@mail.ru>
2026-03-14 23:33:51 +0700

    Обновил фото ключей

--- L3S4/App0.cs
+++ L3S4/App0.cs
@@ -1,33 +1,159 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Threading; 
 
 namespace L3S4
 {
-    public class App0 : INotifyPropertyChanged
+    public class App0 : BaseView
     {
-        
-        private string _version;
-        List<Element> elements; public event PropertyChangedEventHandler PropertyChanged;
+        private bool _capsLook;
+        private InputLanguage _selectLanguage;
+        private string _language;
+        private DispatcherTimer _timer;
+        private string _version = "1.1.1.1";
+        //!!! Т.к обновление списка происходит в наследниках
+        //    то необходимо создать событие в set или метод для открытия окна.
+        //    окно должно открыться когда в elements появятся значения.
+        internal List<Element> elements;
+                                                                
+
+        public string CapsLookGet
+        {
+            get
+            {
+                return this._capsLook ? "Нажата" : "Не нажата";
+            }
+        }
+        public bool CapsLookSet
+        {
+            set
+            {
+                this._capsLook = value;
+                OnPropertyChanged(nameof(CapsLookGet));
+                OnPropertyChanged(nameof(CapsLookSet));
+            }
+        }
+        public string SelectLanguage
+        {
+            get { return this._language; }
+            set
+            {
+                this._language = value;
+                OnPropertyChanged(nameof(SelectLanguage));
+            }
+        }
+
         public App0()
         {
-            Version = "1.1.1.1";
-            elements = new List<Element>();
-            InitLogInFrame("1.1.0.0");
+            Init();
         }
-        private void InitLogInFrame(string version)
+        public App0(string version)
         {
             Version = version;
-            LogInFrame logInFrame = new LogInFrame(this);
-            
+
+            Init();
         }
+        private void Init() {
+            _selectLanguage = InputLanguage.CurrentInputLanguage;
+            SelectLanguage = GetDisplayLanguageName(_selectLanguage); //конверитируем название
+
+            InitializeTimer(100);
+
+            InitLogInFrame();
+        }
+        private void InitializeTimer(int updateTime)
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(updateTime); // Проверка каждые 100 мс
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Проверка состояния CapsLock
+            bool currentCapsLock = IsCapsLockOn();
+            if (currentCapsLock != _capsLook)
+            {
+                CapsLookSet = currentCapsLock;
+            }
+
+            InputLanguage currentLanguage = InputLanguage.CurrentInputLanguage;
+            if (!currentLanguage.Equals(_selectLanguage))
+            {
+                _selectLanguage = currentLanguage;
+                SelectLanguage = GetDisplayLanguageName(currentLanguage);
+
+                OnPropertyChanged(nameof(SelectLanguage));
+            }
+        }
+        
+        private string GetDisplayLanguageName(InputLanguage language)
+        {
+            string cultureName = language.Culture.EnglishName;
+           
+            switch (language.LayoutName)
+            {
+                case "Русская":
+                case "Russian":
+                    return "Русский";
+
+                case "США":
+                case "US":
+                case "English":
+                case "Английский":
+                    return "Английский";
+                default:
+                    
+                    return language.LayoutName;
+            }
+        }
+
+        // Метод для проверки состояния CapsLock
+        private bool IsCapsLockOn()
+        {
+            return Control.IsKeyLocked(Keys.CapsLock);
+        }
+
+        // Метод для ручного обновления
+        public void RefreshKeyboardState()
+        {
+            
+            bool currentCapsLock = IsCapsLockOn();
+            if (currentCapsLock != _capsLook)
+            {
+                CapsLookSet = currentCapsLock;
+            }
+
+            InputLanguage currentLanguage = InputLanguage.CurrentInputLanguage;
+            if (!currentLanguage.Equals(_selectLanguage))
+            {
+                _selectLanguage = currentLanguage;
+                SelectLanguage = GetDisplayLanguageName(currentLanguage);
+            }
+        }
+
+        // Метод для остановки таймера (при закрытии приложения)
+        public void CleanupTimer()
+        {
+            if (_timer != null)
+            {
+                _timer.Stop();
+                _timer.Tick -= Timer_Tick;
+                _timer = null;
+            }
+        }
+        private void InitLogInFrame()
+        {
+            LogInFrame logInFrame = new LogInFrame(this);
+        }
+        public virtual bool CheckPassword(string Name, string Password) { return false; }
         public string Version
         {
-            get { return _version; }
+            get { 
+                return "Версия "+ _version; 
+            }
             set
             {
                 if (_version != value)
@@ -37,11 +163,5 @@ namespace L3S4
                 }
             }
         }
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
     }
 }
-
--- L3S4/App1.cs
+++ L3S4/App1.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+//Раннее связывание.
+
 using AuthLibrary;
 using MenuLibrary;
 
@@ -15,15 +15,36 @@ namespace L3S4
     {
         private AuthLibrary.AuthService authLibrary;
         private MenuLibrary.MenuModel menuModel;    
+        private List<MenuItem> menuItems;
         public App1(string menuPath,string usersPath)
         { 
             this.authLibrary = new AuthService(usersPath);
             this.menuModel = new MenuModel(menuPath);
         }
-        private void Init()
+
+        public override bool CheckPassword(string Name, string Password)
         {
+            Dictionary<string, int> notAveable = authLibrary.Login(Name, Password);
+            if (notAveable != null) {
+                
+
+                menuModel.ApplyPermissions(notAveable);
+                menuItems =  menuModel.GetRootMenu();
+                ConvertMenuItemsAndAddToBase();
+                return true;  
+            }
+            else { return false; }    
             
         }
-
+        private void ConvertMenuItemsAndAddToBase()
+        {
+            List<Element> elements = new List<Element>();
+            foreach (var item in menuItems)
+            {
+                Element element = new Element(item.Level, item.Title, item.MethodName);
+                elements.Add(element);
+            }
+            base.elements = elements;
+        }
     }
 }
