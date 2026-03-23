5741320bb7b4ca318ed06e68d43d99ba17c8c687
kavaler <littlesonofsun@gmail.com>
2026-03-23 22:07:52 +0700

    По идее теперь без реализации в App0 и через elements

--- L3S4-Net8/App0.cs
+++ L3S4-Net8/App0.cs
@@ -1,5 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Windows.Forms;
 using System.Windows.Threading;
 
 namespace L3S4
@@ -16,28 +21,24 @@ namespace L3S4
 
         public event EventHandler ElementsUpdated;
 
-        public string CapsLookGet
-        {
-            get
-            {
-                return this._capsLook ? "Нажата" : "Не нажата";
-            }
-        }
+        public string CapsLookGet => _capsLook ? "Нажата" : "Не нажата";
+
         public bool CapsLookSet
         {
             set
             {
-                this._capsLook = value;
+                _capsLook = value;
                 OnPropertyChanged(nameof(CapsLookGet));
                 OnPropertyChanged(nameof(CapsLookSet));
             }
         }
+
         public string SelectLanguage
         {
-            get { return this._language; }
+            get => _language;
             set
             {
-                this._language = value;
+                _language = value;
                 OnPropertyChanged(nameof(SelectLanguage));
             }
         }
@@ -47,20 +48,22 @@ namespace L3S4
             elements = new List<Element>();
             Init();
         }
+
         public App0(string version)
         {
-            Version = version;
+            _version = version;
             elements = new List<Element>();
             Init();
         }
-        private void Init() {
+
+        private void Init()
+        {
             _selectLanguage = InputLanguage.CurrentInputLanguage;
             SelectLanguage = GetDisplayLanguageName(_selectLanguage);
-
             InitializeTimer(100);
-
             InitLogInFrame();
         }
+
         private void InitializeTimer(int updateTime)
         {
             _timer = new DispatcherTimer();
@@ -73,30 +76,24 @@ namespace L3S4
         {
             bool currentCapsLock = IsCapsLockOn();
             if (currentCapsLock != _capsLook)
-            {
                 CapsLookSet = currentCapsLock;
-            }
 
             InputLanguage currentLanguage = InputLanguage.CurrentInputLanguage;
             if (!currentLanguage.Equals(_selectLanguage))
             {
                 _selectLanguage = currentLanguage;
                 SelectLanguage = GetDisplayLanguageName(currentLanguage);
-
                 OnPropertyChanged(nameof(SelectLanguage));
             }
         }
 
         private string GetDisplayLanguageName(InputLanguage language)
         {
-            string cultureName = language.Culture.EnglishName;
-
             switch (language.LayoutName)
             {
                 case "Русская":
                 case "Russian":
                     return "Русский";
-
                 case "США":
                 case "US":
                 case "English":
@@ -107,18 +104,13 @@ namespace L3S4
             }
         }
 
-        private bool IsCapsLockOn()
-        {
-            return Control.IsKeyLocked(Keys.CapsLock);
-        }
+        private bool IsCapsLockOn() => Control.IsKeyLocked(Keys.CapsLock);
 
         public void RefreshKeyboardState()
         {
             bool currentCapsLock = IsCapsLockOn();
             if (currentCapsLock != _capsLook)
-            {
                 CapsLookSet = currentCapsLock;
-            }
 
             InputLanguage currentLanguage = InputLanguage.CurrentInputLanguage;
             if (!currentLanguage.Equals(_selectLanguage))
@@ -137,33 +129,56 @@ namespace L3S4
                 _timer = null;
             }
         }
+
         private void InitLogInFrame()
         {
-            LogInFrame logInFrame = new LogInFrame(this);
+            _ = new LogInFrame(this);
         }
-        public virtual bool CheckPassword(string Name, string Password)
+
+        public virtual bool CheckPassword(string Name, string Password) => false;
+
+        public string Version => "Версия " + _version;
+
+        // --- Новые методы для работы с элементами меню ---
+        public IReadOnlyList<Element> GetElements() => elements.AsReadOnly();
+
+        public void SetElements(IEnumerable<Element> newElements)
         {
-            return false;
+            elements = newElements?.ToList() ?? new List<Element>();
+            ElementsUpdated?.Invoke(this, EventArgs.Empty);
         }
-        public string Version
+
+        public void InvokeMethod(string methodName)
         {
-            get {
-                return "Версия "+ _version;
-            }
-            set
+            if (string.IsNullOrWhiteSpace(methodName))
             {
-                if (_version != value)
+                System.Windows.MessageBox.Show("Метод не задан", "Информация",
+                    System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
+                return;
+            }
+
+            var mi = GetType().GetMethod(methodName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
+                null, Type.EmptyTypes, null);
+            if (mi != null)
+            {
+                try
                 {
-                    _version = value;
-                    OnPropertyChanged(nameof(Version));
+                    mi.Invoke(this, null);
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show($"Ошибка при вызове метода {methodName}: {ex.Message}",
+                        "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
                 }
             }
         }
 
-        // Пример тестового метода, который можно вызывать из menu.txt (без параметров)
+        // Пример тестового метода
         public void OpenDepartments()
         {
-            System.Windows.MessageBox.Show("Открыть: Отделы", "Действие", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
+            System.Windows.MessageBox.Show("Открыть: Отделы", "Действие",
+                System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
         }
     }
 }
--- L3S4-Net8/App2.cs
+++ L3S4-Net8/App2.cs
@@ -1,96 +1,76 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
-using System.Xml.Linq;
 
 namespace L3S4
 {
-    /// <summary>
-    /// Вариант позднего связывания
-    /// </summary>\
     internal class App2 : App0
     {
-        /*
-         * Introduce
-         * Как работает позднее связываниее?
-         * В C# все просто. Существует класс Assembly который может загружать dll.
-         * На выходе мы получаем что то что можно перебрать,
-         * это что то содержит типы ( то есть все классы из библиотеки)
-         * Выбирая конкретный тип с помощью GetTtpe() можно найти в нем соответствующий метод или свойство
-         * с помощью ProprtyInfo и MethodInfo. Далее объект этих типов (наш метод или свойство) можно 
-         * вызвать с помощью .Invoke(_Объект вызова, параметры);
-         * Также можно создавать объекты соответствующих типов с помощью Activatot.CreateInstance
-         */
         private Type? menu, auth, menuItem;
         private object? menuObj, authObj;
         private IEnumerable? result;
-        private PropertyInfo titleProp, levelProp, accessProp, methodNameProp, childhave;
-        public App2(string menuPath, string usersPath) {
-            try
-            {
-                menu = Assembly.LoadFrom("MenuLibrary.dll").GetType("MenuLibrary.MenuModel");
-                auth = Assembly.LoadFrom("AuthLibrary.dll").GetType("AuthLibrary.AuthService");
-            }
-            catch(Exception ex ) {
-                throw new Exception(ex + " Ошибка загрузки dll в позднем связывании");
-            }  
+        private PropertyInfo? titleProp, levelProp, accessProp, methodNameProp, childProp;
 
-            this.menuObj = Activator.CreateInstance(menu, menuPath);
-            this.authObj = Activator.CreateInstance(auth, usersPath);
+        public App2(string menuPath, string usersPath)
+        {
+            menu = Assembly.LoadFrom("MenuLibrary.dll").GetType("MenuLibrary.MenuModel");
+            auth = Assembly.LoadFrom("AuthLibrary.dll").GetType("AuthLibrary.AuthService");
 
-            result = new List<object>();
+            if (menu == null || auth == null)
+                throw new System.Exception("Не удалось загрузить типы из сборок.");
+
+            menuObj = Activator.CreateInstance(menu, menuPath);
+            authObj = Activator.CreateInstance(auth, usersPath);
         }
-        public override bool CheckPassword(string Name, string Password) {
-            MethodInfo? LoginAuth = auth.GetMethod("Login");
-            MethodInfo? ApplyMenu = menu.GetMethod("ApplyPermissions");
-            MethodInfo? GetRootMenu = menu.GetMethod("GetRootMenu");
 
-            object? logincheck = LoginAuth?.Invoke(authObj, new string[] { Name, Password });
-            if (logincheck is not null)
+        public override bool CheckPassword(string Name, string Password)
+        {
+            var loginAuth = auth?.GetMethod("Login");
+            var applyMenu = menu?.GetMethod("ApplyPermissions");
+            var getRootMenu = menu?.GetMethod("GetRootMenu");
+
+            if (loginAuth == null || applyMenu == null || getRootMenu == null)
+                return false;
+
+            object? loginCheck = loginAuth.Invoke(authObj, new string[] { Name, Password });
+            if (loginCheck != null)
             {
-                ApplyMenu?.Invoke(menuObj,new object[] { logincheck });
-                object? RootMenu = GetRootMenu?.Invoke(menuObj, null);
-                result = RootMenu as IEnumerable;
-                Assembly menuitems = Assembly.LoadFrom("MenuLibrary.dll");
-                menuItem = menuitems.GetType("MenuLibrary.MenuItem");
-                List<Element> elements = new List<Element>();
+                applyMenu.Invoke(menuObj, new object[] { loginCheck });
+                object? rootMenu = getRootMenu.Invoke(menuObj, null);
+                result = rootMenu as IEnumerable;
 
-                titleProp = menuItem.GetProperty("Title");
-                levelProp = menuItem.GetProperty("Level");
-                accessProp = menuItem.GetProperty("Status");
-                methodNameProp = menuItem.GetProperty("MethodName");
-                childhave = menuItem.GetProperty("Children");
+                var menuItemType = Assembly.LoadFrom("MenuLibrary.dll").GetType("MenuLibrary.MenuItem");
+                if (menuItemType == null) return false;
 
-                if (titleProp is not null && levelProp is not null &&
-                     accessProp is not null &&  methodNameProp is not null &&  childhave is not null)
-                {
-                    ConvertMenuItemsAndAddToBase(elements, result);
-                }
-                else
-                {
-                    throw new InvalidOperationException("Не найдено одно из свойств в MenuItem(.dll)");
-                }
-                base.elements = elements;
+                titleProp = menuItemType.GetProperty("Title");
+                levelProp = menuItemType.GetProperty("Level");
+                accessProp = menuItemType.GetProperty("Status");
+                methodNameProp = menuItemType.GetProperty("MethodName");
+                childProp = menuItemType.GetProperty("Children");
+
+                var elements = new List<Element>();
+                ConvertMenuItemsAndAddToBase(elements, result);
+                SetElements(elements);
                 return true;
             }
-            else { return false; }
+            return false;
         }
-        private void ConvertMenuItemsAndAddToBase(List<Element> elements, IEnumerable? ListOfMenuItems) {
-            if (ListOfMenuItems != null)
+
+        private void ConvertMenuItemsAndAddToBase(List<Element> elements, IEnumerable? items)
+        {
+            if (items == null) return;
+            foreach (var item in items)
             {
-                foreach (var item in ListOfMenuItems)
-                {
-                    int Level = (int?)levelProp.GetValue(item) ?? 0;
-                    string Name = titleProp.GetValue(item) as string ?? string.Empty;
-                    int Status = (int?)accessProp.GetValue(item) ?? 0;
-                    string MethodName = methodNameProp.GetValue(item) as string ?? string.Empty;
-                    IEnumerable? listOfMenuItem = childhave.GetValue(item) as IEnumerable ?? null;
-                    Element element = new Element(Level, Name, Status, MethodName);
-                    elements.Add(element);
-                    if (listOfMenuItem != null)
-                    {
-                        ConvertMenuItemsAndAddToBase(elements, listOfMenuItem);
-                    }
-                }
+                int level = (int)(levelProp?.GetValue(item) ?? 0);
+                string name = titleProp?.GetValue(item) as string ?? "";
+                int access = (int)(accessProp?.GetValue(item) ?? 0);
+                string methodName = methodNameProp?.GetValue(item) as string ?? "";
+                var children = childProp?.GetValue(item) as IEnumerable;
+
+                elements.Add(new Element(level, name, access, methodName));
+
+                if (children != null)
+                    ConvertMenuItemsAndAddToBase(elements, children);
             }
         }
     }
