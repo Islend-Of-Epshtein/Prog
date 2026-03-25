4f0b8079d698e34084a2c45addcf6fe4c0b0f503
URETZ <u-beresnev2006@mail.ru>
2026-03-25 11:11:16 +0700

    Create L3S4 tree

--- L1S4/app/src/main/java/lab1/domain/exceptions/DuplicateComponentException.java
+++
@@ -1,7 +0,0 @@
-package lab1.domain.exceptions;
-
-public class DuplicateComponentException extends DomainException {
-    public DuplicateComponentException(String name) {
-        super("Component already exists: " + name);
-    }
-}
--- L1S4/app/src/main/java/lab1/domain/exceptions/ReferenceExistsException.java
+++
@@ -1,7 +0,0 @@
-package lab1.domain.exceptions;
-
-public class ReferenceExistsException extends DomainException {
-    public ReferenceExistsException(String name) {
-        super("Cannot delete component with references: " + name);
-    }
-}
