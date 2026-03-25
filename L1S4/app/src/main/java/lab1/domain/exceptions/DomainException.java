ab87b5022b7d6b8f310b3276af63edc488ed7d6c
URETZ <u-beresnev2006@mail.ru>
2026-03-25 11:09:32 +0700

    Create L2S4 tree2

--- L1S4/app/src/main/java/lab1/domain/exceptions/DomainException.java
+++
@@ -1,7 +0,0 @@
-package lab1.domain.exceptions;
-
-public class DomainException extends RuntimeException {
-    public DomainException(String message) {
-        super(message);
-    }
-}
--- L1S4/src/main/java/lab1/domain/exceptions/InvalidComponentTypeException.java
+++
@@ -1,7 +0,0 @@
-package lab1.domain.exceptions;
-
-public class InvalidComponentTypeException extends DomainException {
-    public InvalidComponentTypeException(String message) {
-        super(message);
-    }
-}
