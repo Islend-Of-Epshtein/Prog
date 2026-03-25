dbec9fbe283e1ab063dbc15133ec9df1f508f479
URETZ <u-beresnev2006@mail.ru>
2026-03-25 11:30:24 +0700

    clean main

--- L1S4/src/main/java/lab1/domain/exceptions/ComponentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package lab1.domain.exceptions;
-
-public class ComponentNotFoundException extends DomainException {
-    public ComponentNotFoundException(String name) {
-        super("Component not found: " + name);
-    }
-}
