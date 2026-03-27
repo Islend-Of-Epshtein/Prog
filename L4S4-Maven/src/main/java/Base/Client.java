2cea27f7161c46f31ed5c7bc9099badd001d13a8
URETZ <u-beresnev2006@mail.ru>
2026-03-27 15:36:35 +0700

    Client и Sever готовы

--- L4S4-Maven/src/main/java/Base/Client.java
+++ L4S4-Maven/src/main/java/Base/Client.java
@@ -4,20 +4,72 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
-
+/// Шаблон клианта
 public class Client {
-    private Socket clientSocket;
-    private PrintWriter in;
-    private BufferedReader out;
-
+    private final Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private ObjectInputStream objIn;
+    private ObjectOutputStream objOut;
+    /// Используем свободный порт и локальный адрес
     public Client() throws IOException
     {
         clientSocket = new Socket("localhost" , 0);
-        in = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())));
+        initBuffer();
     }
+    /// Конструктор с кастомными адресом и портом
     public Client(InetAddress address, int port) throws IOException
     {
         clientSocket = new Socket(address , port);
+        initBuffer();
+    }
+    private void initBuffer() throws IOException {
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())));
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        objOut = new ObjectOutputStream(clientSocket.getOutputStream());
+        objIn = new ObjectInputStream(clientSocket.getInputStream());
     }
 
+    /// Отправить сообщение, а затем получить ответ от сервера
+    public String Write(String str) throws IOException {
+        //отправляем
+        out.write(str);
+        //ждем ответ
+        return in.readLine();
+    }
+    public Object Write(Object obj) throws IOException, ClassNotFoundException {
+        //отправляем
+        objOut.writeObject(obj);
+        objOut.flush();
+        //ждем ответ
+        return objIn.readObject();
+    }
+
+    /// Отправить сообщение, log необходимо установить в true
+    public void Write(String str, boolean log) throws IOException {
+        //отправляем если true
+        if(log) { out.write(str); }
+    }
+
+    /// Прочитать сообщение из буффера от сервера
+    public String Read() throws IOException {
+        return in.readLine();
+    }
+    public Object Read(boolean log) throws ClassNotFoundException, IOException {
+        if(log) {return objIn.readObject(); }
+        return null;
+    }
+
+    /// Проверяет, установлено ли соединение
+    public boolean IsBound(){ return clientSocket.isBound(); }
+    /// Получает адрес сокета
+    public String GetAddress() { return clientSocket.getInetAddress().toString(); }
+    /// Порт сервера, с которым соединен сокет
+    public int GetPort() { return clientSocket.getPort(); }
+    /// Локальный порт данного сокета
+    public int GetLocalPort() { return clientSocket.getLocalPort(); }
+    /// Для любой другой работы с сокетом
+    public Socket GetSocket(){
+        return clientSocket;
+    }
 }
--- L4S4-Maven/src/main/java/Base/Server.java
+++ L4S4-Maven/src/main/java/Base/Server.java
@@ -1,4 +1,77 @@
 package Base;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/// Шаблон сервера
 public class Server {
+    private final ServerSocket serverSocket;
+    private final Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private ObjectInputStream objIn;
+    private ObjectOutputStream objOut;
+
+    /// Используем свободный порт и локальный адрес
+    public Server() throws IOException
+    {
+        serverSocket = new ServerSocket(0);
+        clientSocket = serverSocket.accept();
+        initBuffer();
+    }
+    /// Конструктор с кастомными адресом и портом
+    public Server(int port) throws IOException
+    {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        initBuffer();
+    }
+    private void initBuffer() throws IOException {
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())));
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        objOut = new ObjectOutputStream(clientSocket.getOutputStream());
+        objIn = new ObjectInputStream(clientSocket.getInputStream());
+    }
+    /// Отправить сообщение, а затем получить ответ от сервера
+    public String Write(String str) throws IOException {
+        //отправляем
+        out.write(str);
+        //ждем ответ
+        return in.readLine();
+    }
+    /// Отправить сообщение, log необходимо установить в true
+    public void Write(String str, boolean log) throws IOException {
+        //отправляем если true
+        if(log) { out.write(str); }
+    }
+    public Object Write(Object obj) throws IOException, ClassNotFoundException {
+        //отправляем
+        objOut.writeObject(obj);
+        objOut.flush();
+        //ждем ответ
+        return objIn.readObject();
+    }
+
+    /// Прочитать сообщение из буффера от сервера
+    public String Read() throws IOException {
+        return in.readLine();
+    }
+    /// log = true для чтения объекта из буфера
+    public Object Read(boolean log) throws ClassNotFoundException, IOException {
+        if(log) {return objIn.readObject(); }
+        return null;
+    }
+    /// Проверяет, установлено ли соединение
+    public boolean IsBound(){ return serverSocket.isBound(); }
+    /// Получает адрес сервеара
+    public String GetAddress() { return serverSocket.getInetAddress().toString(); }
+    /// Порт сервера, с которым соединен сервер
+    public int GetPort() { return clientSocket.getPort(); }
+    /// Локальный порт данного сокета
+    public int GetLocalPort() { return clientSocket.getLocalPort(); }
+    /// Для любой другой работы с сокетом
+    public Socket GetSocket(){
+        return clientSocket;
+    }
 }
